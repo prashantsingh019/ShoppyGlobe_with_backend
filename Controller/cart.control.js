8e07b2a0553f9776e5bddcb3393c5c63f16e7e4b
prashantsingh019 <ps731278@gmail.com>
2024-12-23 16:53:10 +0530

    feature making

--- Controller/cart.control.js
+++ Controller/cart.control.js
@@ -1,16 +1,50 @@
-import cartModel from "../Model/cart.model";
+import cartModel from "../Model/cart.model.js";
+import productModel from "../Model/product.model.js";
+
 
 export const addProductToCart = async (req,res) => {
+   try{
+      const {productId,quantity} = req.body;
+      const isExist = await productModel.findById(productId);
+      if(!isExist) return res.status(404).json("product not found");
+      const inCartAlready = await cartModel.findOne({productId:productId})
+      if(inCartAlready){
+         inCartAlready.quantity += quantity;
+         await inCartAlready.save()
+         res.status(200).json("product exist so quantity updated")
+      }else{
+         const cartProduct = new cartModel({
+             productId,
+             quantity,
+         })
+         await cartProduct.save();
+         res.status(200).json("product added to cart")
+      }
+   }catch(err){
+     res.status(500).json(`Internal Server error`);
+   }
+}
+export const updateQuantity = async (req,res) => {
   try{
-     const {productID,quantity} = req.body;
-     const product = new cartModel({
-        productID,
-        quantity,
-     });
-     const save = await product.save();
-     res.status(200).json(product);
+    const{productID,quantity} = req.body;
+    const product = await cartModel.findOne({productID});
+    if(!product) return res.status(404).json(`No such product available`)
+    product.quantity = quantity;
+    await product.save()
+    res.status(200).json(`update quantity: ${product.quantity}`);
   }catch(error){
    res.status(500).json(`Internal Server error`);
   }
-  
 }
+
+export const deleteProduct = async (req,res) => {
+  try{
+    const {productID} = req.body;
+    const findInCart = await cartModel.findOne({productID});
+    const deleteProduct = await cartModel.findByIdAndDelete(findInCart["_id"])
+    res.status(200).json(deleteProduct)
+  }catch(error){
+   res.status(500).json(`Internal Server error`);
+  }
+}
+ 
